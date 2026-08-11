a32e91e0c38bc4d0778328c2e1e685a6fa373e4c
MiTbHandari2000 <meeetbhandari786@gmail.com>
2026-08-11 11:55:05 +0530

    first commit

---
+++ assignment 1/hello.py
@@ -0,0 +1,16 @@
+#programm that print name,age,city in a single line
+name = input("Enter your name: ")
+age = input("Enter your age: ") 
+city = input("Enter your city: ")
+print("Name:", name, "Age:", age, "City:", city)
+
+#take two numbers from user and print their sum
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+sum = num1 + num2
+print("The sum of", num1, "and", num2, "is:", sum)
+
+#convert temperature from Celsius to Fahrenheit
+celsius = float(input("Enter temperature in Celsius: "))
+fahrenheit = (celsius * 1.8) + 32
+print ("Temperature in Fahrenheit:", fahrenheit)
