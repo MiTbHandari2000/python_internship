f9cb137c4484a3ac87a753404a38cbd5ffd6aeaa
MiTbHandari2000 <meeetbhandari786@gmail.com>
2026-08-11 12:55:39 +0530

    added the first assignment

--- assignment 1/hello.py
+++ assignment 1/hello.py
@@ -14,3 +14,49 @@ print("The sum of", num1, "and", num2, "is:", sum)
 celsius = float(input("Enter temperature in Celsius: "))
 fahrenheit = (celsius * 1.8) + 32
 print ("Temperature in Fahrenheit:", fahrenheit)
+
+#store your name in variable and print in uppercase
+name = input("Enter your name: ")
+name_upper = name.upper()
+print("Your name in uppercase is:", name_upper)
+
+#calculate age of from birth year
+
+birth_year = int(input("Enter your birth year: "))
+
+from datetime import date 
+current_year = date.today().year
+age = current_year - birth_year
+print("Your age is:", age)
+
+#swap the values of two variables
+a = int(input("Enter first number: "))  
+b = int(input("Enter second number: "))
+
+a ,b = b, a
+print(a,b)
+
+#area of rectangle
+length = float(input("Enter length of rectangle: "))
+width = float(input("Enter width of rectangle: "))
+
+area = length * width
+print("Area of rectangle is:", area)
+
+#check weather number is positive or negative
+number = float(input("Enter a number: "))
+
+if number>0:
+    print("The number is positive.")
+elif number<0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
+
+#calculate average of two numbers
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+avg_value = (num1 + num2) / 2
+print("The average of", num1, "and", num2, "is:", avg_value)
